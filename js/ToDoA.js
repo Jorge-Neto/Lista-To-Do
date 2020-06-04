38c42968f741226ba98d6e0db21405392782a598
Jorge Neto <neto_tunas@hotmail.com>
2020-06-04 13:38:15 -0300

    Dando funcionalidade ao botao adicionar

--- js/ToDoA.js
+++ js/ToDoA.js
@@ -1,5 +1,34 @@
 var txtTarefa = document.querySelector('#txtTarefa');
 var btnAdd = document.querySelector('#btnAdd');
+var tableToDo = document.querySelector('#tableToDo tbody');
 
-console.log(txtTarefa);
-console.log(btnAdd);
+console.log(tableToDo);
+
+
+btnAdd.onclick = function (){
+    //innerHTML substitui conteúdo atual pelo conteudo entre ''  
+    //Variável criada para checar a input apenas uma vez
+    var tarefa = txtTarefa.value.trim();
+    //trim remove os espaços em branco
+    if (tarefa === '') {
+        alert('Você deve informar uma tarefa');    
+    }else{
+        //variaveis para linha com 3 colunas
+        var linha = document.createElement('tr');
+        var celulaNumero = document.createElement('th');
+        var celulaTarefa = document.createElement('td');
+        var celulaRemover = document.createElement('td');
+
+        //celulaTtarefa recebe o input armazenado em tarefa
+        celulaTarefa.appendChild(document.createTextNode(tarefa));
+        //Coloca as variáveis dentro da linha conforme a ordem
+        linha.appendChild(celulaNumero);
+        linha.appendChild(celulaTarefa);
+        linha.appendChild(celulaRemover);
+        //coloca a linha na tabela
+        tableToDo.appendChild(linha);
+
+        console.log(linha);
+    }
+    
+}
